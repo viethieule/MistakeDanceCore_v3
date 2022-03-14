19265e062651d680a6e4a29f40da5f0f9f53e2f1
viethieule <hieu.le@practicehub.org>
2022-03-14 18:01:09 +0700

    wip

--- Application/Memberships/MembershipDTO.cs
+++ Application/Memberships/MembershipDTO.cs
@@ -5,5 +5,12 @@ namespace Application.Memberships
         public int MemberId { get; set; }
         public int RemainingSessions { get; set; }
         public DateTime ExpiryDate { get; set; }
+        public bool IsExpired
+        {
+            get
+            {
+                return RemainingSessions <= 0 || ExpiryDate < DateTime.Now.Date;
+            }
+        }
     }
 }
---
+++ Application/Registrations/CreateRegistrationService.cs
@@ -0,0 +1,36 @@
+using Application.Common;
+using Application.Common.Interfaces;
+using Application.Memberships;
+using Application.Packages;
+
+namespace Application.Registrations
+{
+    public class CreateRegistrationRq : BaseRequest
+    {
+        public int MemberId { get; set; }
+        public int SessionId { get; set; }
+    }
+
+    public class CreateRegistrationRs : BaseResponse
+    {
+    }
+
+    public class CreateRegistrationService : TransactionalService<CreateRegistrationRq, CreateRegistrationRs>
+    {
+        private readonly MembershipDTC _membershipDTC;
+        private readonly PackageDTC _packageDTC;
+        private readonly RegistrationDTC _registrationDTC;
+        
+        public CreateRegistrationService(IMistakeDanceDbContext mistakeDanceDbContext, MembershipDTC membershipDTC, PackageDTC packageDTC, RegistrationDTC registrationDTC) : base(mistakeDanceDbContext)
+        {
+            this._registrationDTC = registrationDTC;
+            this._packageDTC = packageDTC;
+            this._membershipDTC = membershipDTC;
+        }
+
+        protected override Task<CreateRegistrationRs> RunTransactionalAsync(CreateRegistrationRq rq)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
