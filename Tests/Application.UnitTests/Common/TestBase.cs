ee43057875a1c57676c6401f26cad222103e4639
Hieu Le <Le.Hieu@steadfasttech.com.au>
2022-05-09 00:00:33 +0700

    wip

---
+++ Tests/Application.UnitTests/Common/TestBase.cs
@@ -0,0 +1,11 @@
+public class TestBase : IDisposable
+{
+    public TestBase()
+    {    
+    }
+
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Tests/Application.UnitTests/Schedules/CreateScheduleTests.cs
@@ -0,0 +1,19 @@
+using Xunit;
+
+namespace Application.UnitTests.Schedules;
+
+public class CreateScheduleTests : TestBase
+{
+    [Fact]
+    public void Handle_GivenScheduleForm_CreatesSchedule()
+    {
+        // MistakeDanceDbContext context = new MistakeDanceDbContext();
+        // CreateScheduleRq rq = new CreateScheduleRq()
+        // {
+
+        // };
+
+
+        // var service = new CreateScheduleService();
+    }
+}
