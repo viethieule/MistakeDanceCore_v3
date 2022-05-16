4d0c9c248f6738b1aa9aca377d3dcbe6009f2db6
Hieu Le <Le.Hieu@steadfasttech.com.au>
2022-05-16 23:59:53 +0700

    wip

--- API/Controllers/AuthenticationController.cs
+++ API/Controllers/AuthenticationController.cs
@@ -10,11 +10,13 @@ namespace API.Controllers
         {
         }
 
+        [HttpPost]
         public async Task<IActionResult> Login(LoginRq rq)
         {
             return Ok(await this.RunAsync<LoginService, LoginRq, LoginRs>(rq));
         }
 
+        [HttpPost]
         public async Task<IActionResult> RefreshToken(RefreshTokenRq rq)
         {
             return Ok(await this.RunAsync<RefreshTokenService, RefreshTokenRq, RefreshTokenRs>(rq));
