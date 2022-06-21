0f72ce31a66a7f1c486c0aca3010ce8f3bf48550
Hieu Le <Le.Hieu@steadfasttech.com.au>
2022-06-21 23:00:57 +0700

    wip

--- Tests/Application.UnitTests/Common/TestConstants.cs
+++ Tests/Application.UnitTests/Common/TestConstants.cs
@@ -23,8 +23,6 @@ namespace Application.UnitTests.Common
         public const int CLASS_2_ID = 2;
         public const string CLASS_2_NAME = "Test class 2 name";
 
-        public const int SCHEDULE_1_ID = 1;
-
         public const int MEMBER_1_ID = 1;
         public const int MEMBER_1_PACKAGE_ID = 1;
 
